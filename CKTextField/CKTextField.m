70001d6088552055a8f3cccb59fa15084e4f1402
Christian Klaproth <christian.klaproth@gebit.de>
2016-09-29 10:22:02 +0200

    Fix button misplacement. #21

--- CKTextField/CKTextField.m
+++ CKTextField/CKTextField.m
@@ -85,8 +85,10 @@ static NSString* VALIDATION_TYPE_TEXT = @"text";
     if (self.placeholderLabel) {
         self.placeholderLabel.frame = CGRectMake(7.0, 0.0, self.bounds.size.width - 14.0, self.bounds.size.height);
     }
-    if (self.originalTextAlignment == NSTextAlignmentCenter) {
-        self.leftView.frame = CGRectMake(0.0, 0.0, self.bounds.size.width / 2 - 8, self.bounds.size.height);
+    if (self.acceptButton) {
+        CGFloat buttonWidth = self.bounds.size.height - 4.0;
+        self.acceptButton.frame = CGRectMake(self.bounds.size.width - buttonWidth - 2.0, 2.0, self.bounds.size.height - 4.0, self.bounds.size.height - 4.0);
+        self.acceptButton.layer.cornerRadius = (self.bounds.size.height - 4.0) / 2;
     }
 }
 
